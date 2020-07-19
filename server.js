2fbf8c1eb7433d2ef20316224be73809516b987a
CleanCodeCreature <lambertlee410@gmail.com>
2020-07-19 20:04:08 +0530

    version 1:ready for deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 
 connectDB()
 
@@ -18,5 +19,18 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/post'))
 
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+
+}
+
+
+
 const PORT = process.env.PORT || 5500;
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
